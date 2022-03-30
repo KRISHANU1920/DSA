419cf2e43918c84c0fbcf55b7537db74e99284c4
KRISHANU1920 <krishanu1920@gmail.com>
2022-03-30 17:00:23 +0530

    Height of Binary Tree

---
+++ Tree/BinaryTree/heightOfTree.cpp
@@ -0,0 +1,22 @@
+// Question Link : https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1
+
+int height(struct Node* node)
+{
+    // base case
+    if(node == NULL)
+        return 0;
+    
+    int left = height(node -> left);
+    int right = height(node -> right);
+    
+    int ans = max(left, right) + 1;
+    
+    return ans;
+}
+
+/*
+
+Time Complexity: O(N)
+Space Complexity: O(height)
+
+*/
