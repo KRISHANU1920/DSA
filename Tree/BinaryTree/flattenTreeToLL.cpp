cb1a06e6f22d099f103f842e9d827e81d7045cad
KRISHANU1920 <krishanu1920@gmail.com>
2022-04-08 17:49:54 +0530

    Flatten Tree into LinkedList

--- Tree/BinaryTree/flattenTreeToLL.cpp
+++ Tree/BinaryTree/flattenTreeToLL.cpp
@@ -1,5 +1,40 @@
 // Question Link : https://practice.geeksforgeeks.org/problems/flatten-binary-tree-to-linked-list/1/
 
+
+// Approach 1: Recursion (using extra space)
+Node* solve(Node *root)
+{
+    if(root -> left == NULL && root -> right == NULL)
+        return root;
+    
+    if(root -> left)
+    {
+        Node *leftAns = solve(root -> left);
+        leftAns -> right = root -> right;
+        root -> right = root -> left;
+        root -> left = NULL;
+    }
+    
+    if(root -> right)
+    {
+        Node *rightAns = solve(root -> right);
+        return rightAns;
+    }
+}
+void flatten(Node *root)
+{
+    solve(root);
+}
+
+/*
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+
+*/
+
+
+// Approach 2: Using Morris Traversal (Constant Space)
 void flatten(Node *root)
 {
     Node *current, *pred;
