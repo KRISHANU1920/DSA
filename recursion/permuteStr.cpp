0992224ee68dc6c82cb639849739fa5b95adb74a
KRISHANU1920 <krishanu1920@gmail.com>
2022-05-31 21:11:19 +0530

    Permutations of array/string

--- recursion/permuteStr.cpp
+++ recursion/permuteStr.cpp
@@ -1,5 +1,7 @@
 // Question Link : https://leetcode.com/problems/permutations/
 
+
+// Approach 1: optimized [TC-O(N! * N) and SC-O(N!)]
 void solve(string S, int index, vector<string>& ans)
 {
     // base case
@@ -23,3 +25,43 @@ vector<string>find_permutation(string S)
     sort(ans.begin(), ans.end());
     return ans;
 }
+
+
+
+// Approach 2: using mapping  [TC-O(N! * N) and SC-O(N)]
+void solve(vector<int> &arr, vector<int> &ds, vector<vector<int>> &ans, int freq[])
+{
+    if(ds.size() == arr.size())
+    {
+        ans.push_back(ds);
+        return ;
+    }
+    
+    for(int i=0; i<arr.size(); i++)
+    {
+        if(!freq[i])
+        {
+            ds.push_back(arr[i]);
+            freq[i] = 1;
+            
+            solve(arr, ds, ans, freq);
+            
+            freq[i] = 0;
+            ds.pop_back();
+        }
+    }
+}
+
+vector<vector<int>> permute(vector<int>& nums) 
+{
+    vector<vector<int>> ans;
+    vector<int> ds;
+    int freq[nums.size()];
+    
+    for(int i=0; i<nums.size(); i++)
+        freq[i] = 0;
+    
+    solve(nums, ds, ans, freq);
+    return ans;
+}
+
