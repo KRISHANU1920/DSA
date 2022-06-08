af207886ac62c603c1ca1e249133cf8e36809e57
KRISHANU1920 <krishanu1920@gmail.com>
2022-06-08 17:18:41 +0530

    Ninja's Training 2D DP

---
+++ DP/5_ninjaTrain2D.cpp
@@ -0,0 +1,125 @@
+// Recursive Approach: [TC-O(3^(N*4)) and SC-O(N)]
+int solve(int day, int last, vector<vector<int>> &points)
+{
+    if(day == 0)
+    {
+        int maxi = 0;
+        for(int task=0; task < 3; task++)
+        {
+            if(task != last)
+                maxi = max(maxi, points[day][task]);
+        }
+        return maxi;
+    }
+    
+    int maxi = 0;
+    for(int task=0; task < 3; task++)
+    {
+        if(task != last)
+        {
+            int point = points[day][task] + solve(day-1, task, points);
+            maxi = max(maxi, point);
+        }
+    }
+    return maxi;
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    return solve(n-1, 3, points);
+}
+
+
+// Memoization Approach: [TC-O((N*4)*3) and SC-(O(N)+O(N*4))]
+int solve(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp)
+{
+    if(day == 0)
+    {
+        int maxi = 0;
+        for(int task=0; task < 3; task++)
+        {
+            if(task != last)
+                maxi = max(maxi, points[day][task]);
+        }
+        return maxi;
+    }
+    
+    if(dp[day][last] != -1)
+        return dp[day][last];
+    
+    int maxi = 0;
+    for(int task=0; task < 3; task++)
+    {
+        if(task != last)
+        {
+            int point = points[day][task] + solve(day-1, task, points, dp);
+            maxi = max(maxi, point);
+        }
+    }
+    return dp[day][last] = maxi;
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector<int>> dp(n, vector<int> (4, -1));
+    return solve(n-1, 3, points, dp);
+}
+
+// Tabulation Approach: [TC-O((N*4)*3) and SC-O(N*4)]
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector<int>> dp(n, vector<int> (4, -1));
+    
+    dp[0][0] = max(points[0][1], points[0][2]);
+    dp[0][1] = max(points[0][0], points[0][2]);
+    dp[0][2] = max(points[0][0], points[0][1]);
+    dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+    
+    for(int day=1; day < n; day++)
+    {
+        for(int last=0; last < 4; last++)
+        {
+            dp[day][last] = 0;
+            for(int task=0; task < 3; task++)
+            {
+                if(task != last)
+                {
+                    int point = points[day][task] + dp[day-1][task];
+                    dp[day][last] = max(dp[day][last], point);
+                }
+            }
+        }
+    }
+    return dp[n-1][3];
+}
+
+
+// Space Optimization Approach: [TC-O((N*4)*3) and SC-O(4+4) ~ O(1)]
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<int> prev(4, -1);
+    
+    prev[0] = max(points[0][1], points[0][2]);
+    prev[1] = max(points[0][0], points[0][2]);
+    prev[2] = max(points[0][0], points[0][1]);
+    prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
+    
+    for(int day=1; day < n; day++)
+    {
+        vector<int> temp(4, 0);
+        for(int last=0; last < 4; last++)
+        {
+            temp[last] = 0;
+            for(int task=0; task < 3; task++)
+            {
+                if(task != last)
+                {
+                    int point = points[day][task] + prev[task];
+                    temp[last] = max(temp[last], point);
+                }
+            }
+        }
+        prev = temp;
+    }
+    return prev[3];
+}
