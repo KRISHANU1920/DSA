11e8a31e66d3675c200f626e5e3131ede8ed403b
KRISHANU1920 <krishanu1920@gmail.com>
2022-06-17 18:24:06 +0530

    Longest Common Subsequence

--- DP/21_LCS.cpp
+++ DP/21_LCS.cpp
@@ -99,3 +99,31 @@ int lcs(int N, int M, string s1, string s2)
     }
     return prev[M];
 }
+
+
+
+// Printing the LCS  [TC-O(N*M)+O(N+M)backtracking  and SC-O(N*M)+O(len)]
+// extra code for Tabulation approach
+string ans = "";
+int len = dp[N][M];
+
+for(int i=0; i<len; i++)
+    ans += '$';
+
+int index = len-1;
+int i=N, j=M;
+while(i>0 && j>0)
+{
+    if(s1[i-1] == s2[j-1])
+    {
+        ans[index] = s1[i-1];
+        index--;
+        i--;
+        j--;
+    }
+    else if(dp[i-1][j] > dp[i][j-1])
+        i--;
+    else
+        j--;
+}
+return ans;
